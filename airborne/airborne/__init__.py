886f2ef787f35d41e21d047659791253e175a4e3
KomisarMariia <mkomisar@bcdtriptech.com>
2024-12-03 14:23:07 +0100

    airborne

---
+++ airborne/airborne/__init__.py
@@ -0,0 +1,16 @@
+"""
+========
+airborne
+========
+
+GetGoing hotels frontend.
+
+"""
+
+__version__ = '8.67'
+
+CONTEXT_GLOBAL_OVERRIDE = {
+    'release': __version__,
+    'service': 'airborne',
+    'version': __version__,
+}
